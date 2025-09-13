9411940389961ad3f4458a752538eabc5bffb27d
NerfHP <neonman396@gmail.com>
2025-09-14 00:40:10 +0530

    Code

--- client/src/components/shared/ParticleBackground.tsx
+++ client/src/components/shared/ParticleBackground.tsx
@@ -239,7 +239,7 @@ const ParticleBackground: React.FC = () => {
             width: '100%',
             height: '100%',
             zIndex: -1, 
-            backgroundColor: '#FDFBF5'
+            backgroundColor: '#FDF4DC'
         }} 
     />
   );
